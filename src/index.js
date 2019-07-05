8779a24a521fa899453b43b3afc9eeb1803c170a
lachman <latch@sysint.biz>
2019-07-05 11:09:50 +0100

    file changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+    BrowserRouter as Router,
+    Link,
+    NavLink,
+    Route
+} from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Navbar>
+                    {/* "Link" in brand component since just redirect is needed */}
+                    <Navbar.Brand as={NavLink} to="/">
+                        Brand link
+                    </Navbar.Brand>
+                    <Nav>
+                        {/* "NavLink" here since "active" class styling is needed */}
+                        <Nav.Link as={NavLink} to="/" exact>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/another">
+                            Another
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/onemore">
+                            One More
+                        </Nav.Link>
+                    </Nav>
+                </Navbar>
+                <Route path="/" exact component={Home} />
+                <Route path="/another" exact component={Another} />
+                <Route path="/onemore" exact component={Onemore} />
+            </div>
+        </Router>
+    );
+}
+
+function Home() {
+    return <div>Home page</div>;
+}
+function Another() {
+    return <div>Another page</div>;
+}
+function Onemore() {
+    return <div>Onemore page</div>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
